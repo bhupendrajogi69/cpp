db220ab4dac9ceb6e33c53a735fe0f08e6163781
bhupendrajogi69 <51480269+bhupendrajogi69@users.noreply.github.com>
2023-11-09 19:32:03 +0530

    Bhupendra Jogi

--- circleareaperi.cpp
+++ circleareaperi.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
-#define PI 3.141
-int main()
-{
-	float R, A, P;
-	cout << "Enter Radius of circle: ";
-	cin>> R;
-	A = PI*R*R;
-	P = 2*PI*R;
-	cout << "\nArea is: " << A;
-	cout << "\nPerimeter is: " << P;
-	return 0;
+
+int main() {
+    double radius;
+
+    // Prompt the user to enter the radius of the circle
+    cout << "Enter the radius of the circle: ";
+    cin >> radius;
+
+    // Calculate the perimeter and area of the circle
+    double perimeter = 2 * M_PI * radius;
+    double area = M_PI * pow(radius, 2);
+
+    // Calculate the volume of the sphere with the same radius
+    double volume = (4.0 / 3.0) * M_PI * pow(radius, 3);
+
+    // Display the results
+    cout << "Perimeter of the circle: " << perimeter << endl;
+    cout << "Area of the circle: " << area << endl;
+    cout << "Volume of the sphere with the same radius: " << volume << endl;
+
+    return 0;
 }
